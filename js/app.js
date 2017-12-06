73ecb81f8008070b0741be94605b33fb3ffba41d
ProyectoGC2017 <ProyectoGC2017@outlook.es>
2017-12-05 19:24:42 -0600

    capturar, envento submit boton Suscribir

---
+++ js/app.js
@@ -0,0 +1,7 @@
+const $form = $('#suscribeForm')
+const $formGroup = $form.find('.form-group')
+const $formControl = $formGroup.find('.form-control')
+
+$form.submit(function (ev) {
+	ev.preventDefault()
+})
