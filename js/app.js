76175d6da42f4c628ce3a4ced264f19231eb0f38
ProyectoGC2017 <ProyectoGC2017@outlook.es>
2017-12-05 23:46:10 -0600

    Valida los tipo de error, apartir numero random

--- js/app.js
+++ js/app.js
@@ -4,4 +4,15 @@ const $formControl = $formGroup.find('.form-control')
 
 $form.submit(function (ev) {
 	ev.preventDefault()
+
+	const error = Math.random() > 0.5
+	var el;
+	if(error){
+		el = $('<div>',{ html: 'Ha ocurrido un error'})
+	} else{
+		el = $('<div>',{ html: 'Te enviaremos todas las novedades'})
+	}
+
+	el.addClass('form-control-feedback')
+	$formGroup.append(el)
 })
